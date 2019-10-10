a6f92e8e8b10ade1b8f2e3a928507be8faf81359
lucatedesco80 <lucatedesco80@gmail.com>
2019-10-10 12:29:08 +0000

    introduzionenode

--- EXPRESS_STATIC_FILES/app.js
+++ EXPRESS_STATIC_FILES/app.js
@@ -1,5 +1,20 @@
+var express = require('express');
+var app = express();
 const path = require('path');
 
-app.get('/',function(req,res){
- res.sendFile(path.join(__dirname+'/index.html')); 
+app.listen(3000, function () {
+  console.lo('Example app listening on port 3000!');
+});
+
+
+
+
+app.get('/',funcion(req,res){
+ res.sendFile(path.join(__dirname+'/index.html'));
+});
+app.get('/about',function(req,res){
+ res.sendFile(path.join(__dirname+'/about.html'));
+});
+app.get('/sitemap',function(req,res){
+ res.sendFile(path.join(__dirname+'/sitemap.html'));
 });
